91cda38c7b290258238f68021f85ef5a3d263ed9
JohnEcn <Giannhs.econ@hotmail.com>
2021-03-01 20:14:28 +0200

    Removed code that checks if user is authorized because user auth happens in index.php

--- src/Model/userAuth/checkAuth.php
+++ src/Model/userAuth/checkAuth.php
@@ -6,6 +6,14 @@ function getUserID($token)
 {    
     $dbConnection = new DBconnect();
     $userID = $dbConnection->retrieveUserID($token);
+    if(isset($userID[0]['userID']))
+    {
+        return $userID[0]['userID'];
+    }
+    else
+    {
+        return NULL;
+    }
     return $userID[0]['userID'];
 }
 
--- src/Model/userAuth/logOut.php
+++ src/Model/userAuth/logOut.php
@@ -1,19 +1,11 @@
 <?php
 require_once("userDB.php");
 
-function userLogout($token){
-    $dbConnection = new DBconnect();
-    $user = $dbConnection->retrieveUserID($token);
-
-    if(isset($user[0]['userID']))
-    {           
-        $dbConnection->insertToken($user[0]['userID'],NULL);
-        return "User authentication ended";
-    }
-    else
-    {
-        return "Unathorized user";
-    }
+function userLogout($userID)
+{
+    $dbConnection = new DBconnect();           
+    $dbConnection->insertToken($userID,NULL);    
+    return "User auth ended";    
 }
 
 
