b780785ff082f1c321eaf6f267066d22bfa15958
JohnEcn <Giannhs.econ@hotmail.com>
2021-03-12 22:23:35 +0200

    Added function that makes visible the content of the left section only after all css files are loaded

--- src/Controller/userPanel/leftSectionController.js
+++ src/Controller/userPanel/leftSectionController.js
@@ -1,6 +1,14 @@
 function displayContent(statusCode,responseBody)
 {
     let leftContainer = document.getElementById("leftSection");
-    leftContainer.innerHTML = responseBody;
+    leftContainer.style.visibility="hidden";
+    leftContainer.innerHTML = responseBody;   
+    displayExerciseList(); 
+}
+function doneLoading()
+{
+    console.log("swiss");
+    let leftContainer = document.getElementById("leftSection");
+    leftContainer.style.visibility='initial';
     enableButton("addWorkoutBut");
 }
