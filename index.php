e7cfa92c4c3d188f2b0e3a7baafd8d39b243f02d
JohnEcn <Giannhs.econ@hotmail.com>
2021-03-20 19:04:01 +0200

    Designed landing page with log in and sign up form, no functionality yet

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
 
-    echo "landing page";
 
-    //header("Location: src/api");
 
 ?>
 <!DOCTYPE html>
@@ -12,9 +10,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./src/View/landingPage/css/landingStructure.css">
+    <link rel="stylesheet" href="./src/View/landingPage/css/navBar.css">
+    <link rel="stylesheet" href="./src/View/landingPage/css/topHorizSection.css">
+    <link rel="stylesheet" href="./src/View/landingPage/css/authContainer.css">
+
 </head>
 <body>
-    
-    <script src="./src/Controller/clientEndpoint/endPoint.js"></script>
+    <landingPage id='landingPage'>
+        <div id='navBar'>
+            <div id='navLeft'><span id='appTitle'>WorkoutApp    </span></div>
+            <div id='navRight'>
+                <ul id='navLinks'>
+                    <li class='navlink'>Whatever</li>
+                    <li class='navlink'>Whatever</li>
+                    <li class='navlink'>Whatever</li>
+                    <li class='navlink'>Whatever</li>
+                </ul>                
+            </div>            
+        </div>
+        <div id='topHorizSect'>
+            <div id='topHorizSect_left'>
+                <div id='mainTextContainer'>
+                    <h1 id='mainTitle'><span>Organize</span> and <span>track</span> your workouts.</h1>
+                     <h3 class='secondaryTitles'>&bull; Visualize your progress.</h3>
+                     <h3 class='secondaryTitles'>&bull; Compare yourself to others.</h3>
+                     <h3 class='secondaryTitles'>&bull; Improve.</h3>
+                     <input type="submit" value="Try a demo" id="demoButton">
+                </div>       
+            </div>
+            <div id='topHorizSect_right'>
+                <div id='userAuthContainer'>
+                    <input type='text' id='username' class='authInput' title ="migga" placeholder='Username' name='fname'><br><br>
+                    <input type='password' id='password' class='authInput' 
+                    placeholder='Password' name='lname'><br><br>
+                    <input type='submit' value='Log in' id='loginButt'>
+                    <span id='signUpText' onclick='signUpForm();'>Sign up!</span>                     
+                    <span id='errorMessage'></span>  
+                </div>
+            </div>
+            </div>
+        <div id='bottomHorizSect'>  </div>    
+    </landingPage>  
+    <script src="./src/Controller/landingPage/userAuthForms.js"></script>  
 </body>
 </html>
---
+++ src/Controller/landingPage/userAuthForms.js
@@ -0,0 +1,33 @@
+function signUpForm()
+{
+    let container = document.getElementById("userAuthContainer");
+    
+    container.style.height = "85%";
+    container.style.width = "60%";
+    container.style.transition = "all linear 150ms";
+    container.innerHTML = "";
+    
+    let signUpForm = "<input type='text' id='username' class='authInput' placeholder='Username' name='fname'><br>"+
+    "<input type='text' id='email' class='authInput'placeholder='Email' name='lname'><br>"+
+    "<input type='password' id='password' class='authInput' placeholder='Password' name='fname'><br>"+
+    "<input type='password' id='passwordConf' class='authInput'placeholder='Confirm Password' name='lname'><br><br></br>"+
+    "<input type='submit' value='Sign Up' id='signUpButt'><span id='signUpText' onclick='logInForm();'>Alreay a user ?</span><span id='errorMessage'></span> ";
+    setTimeout(() => {
+        container.innerHTML = signUpForm;        
+    }, 150);  
+}
+function logInForm()
+{
+    let container = document.getElementById("userAuthContainer");
+    
+    container.style.height = "60%";
+    container.style.width = "50%";
+    container.style.transition = "all linear 150ms";
+    container.innerHTML = "";
+    
+    let logInForm = "<input type='text' id='fname' class='authInput' placeholder='Username' name='fname'><br><br><input type='password  ' id='lname' class='authInput'placeholder='Password' name='lname'><br><br><input type='submit' value='Log in' id='loginButt'><span id='signUpText' onclick='signUpForm();'>Sign up!</span><span id='errorMessage'></span> ";
+    setTimeout(() => {
+        container.innerHTML = logInForm;      
+    }, 150);  
+
+}
