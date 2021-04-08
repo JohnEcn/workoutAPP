d8c8893277cbd3e3baca77497ef2a96faa10ee09
JohnEcn <Giannhs.econ@hotmail.com>
2021-04-08 21:59:50 +0300

    Fixed design flaw in sql insert queries regarding the auto incremeted ids

--- src/Model/userWorkouts/workoutDB.php
+++ src/Model/userWorkouts/workoutDB.php
@@ -20,11 +20,10 @@ class workoutDB
         $statement->bindParam(1,$userID,PDO::PARAM_INT);
         $statement->execute();
 
-        //Selects the routine IDs that point to the current userID and picks the biggest num
-        $statement = $this->pdo->prepare("SELECT MAX(routineID) FROM `userroutine` WHERE userID = ?");
-        $statement->bindParam(1,$userID,PDO::PARAM_INT);
+        //Selects the recently created exerciseID
+        $statement = $this->pdo->prepare("SELECT @@IDENTITY");
         $statement->execute();
-        $routineID = $statement->fetchAll(PDO::FETCH_COLUMN)[0];
+        $routineID = $statement->fetchAll(PDO::FETCH_COLUMN)[0]; 
 
         //Inserts into routine table the routineID , routineName 
         $statement = $this->pdo->prepare("INSERT INTO `routine` (`routineID`, `routineName`) VALUES (?, ?)");
@@ -40,13 +39,12 @@ class workoutDB
         $statement = $this->pdo->prepare("INSERT INTO `routineexercise` (`routineID`) VALUES (? )");
         $statement->bindParam(1,$routineID,PDO::PARAM_INT);
         $statement->execute();
-
-        //Selects the recently created exerciseID that point to the current routineID
-        $statement = $this->pdo->prepare("SELECT MAX(exerciseID) FROM `routineexercise` WHERE routineID = ?");
-        $statement->bindParam(1,$routineID,PDO::PARAM_INT);
+        
+        //Selects the recently created exerciseID
+        $statement = $this->pdo->prepare("SELECT @@IDENTITY");
         $statement->execute();
-        $exerciseID = $statement->fetchAll(PDO::FETCH_COLUMN)[0];
-
+        $exerciseID = $statement->fetchAll(PDO::FETCH_COLUMN)[0]; 
+       
         //Inserting the exercise into the exercise table with the recently created exerciseID
         $exerciseName = $exercise['name'];
         $exerciseSets = $exercise['sets'];
